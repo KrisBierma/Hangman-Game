4854ac810054d39fc91cc2c3a361d08389effc06
Kris <kris.bierma@gmail.com>
2018-02-10 10:02:40 -0600

    working on Game Over function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,17 @@ var words = ["blue", "yellow", "orange", "silver", "bronze", "periwinkle", "maho
 
 
     var wins = 0;
-    var guesses = 15;
+    var numGuesses = 15;
+    document.getElementById("num-guesses").innerHTML = numGuesses;
     var lettersGuessed = [];
+    //var lettersGuessed = "";
     var currentWord = words[Math.floor(Math.random() * words.length)]; //randomly chooses a word from the words array
     console.log(currentWord); //delete later -- shows current word
     var currentBlank = []; //starts the number of _ at 0
     var wordToArray = currentWord.split(""); //splits the current word into an array, with one letter per space
     console.log(wordToArray); //delete later -- shows current word as an array, one letter per space
+    var gameOver = currentWord.length; //starts the letter count = to current word length
+    console.log(gameOver);
 
     //makes the number of _ for the current word on the board (but doesn't write it yet)
     for (i = 0; i<currentWord.length; i++){ 
@@ -19,37 +23,47 @@ var words = ["blue", "yellow", "orange", "silver", "bronze", "periwinkle", "maho
     //console.log(currentBlank.join(" "));
 
 //writes the number of _ for the current word on the board
-    currentBlank= currentBlank.join(" ");
+    //currentBlank= currentBlank.join(" ");
+    console.log(currentBlank);//shows spaces on the screen
     document.getElementById("current-blank").innerHTML = currentBlank;
-    //console.log(playGame.key); //delete later
 
-//get the initial key press to start the game
+//get a letter; start the game
 document.onkeyup = function (playGame){
-    console.log(playGame.key); //delete later
 
+    var letter = 0;
+  
     //check the key to each letter in the current word
-    for(var j = 0; j<words.length; j++){
+    for(var j = 0; j<currentWord.length; j++){
         if (playGame.key.toLowerCase() === wordToArray[j]){
-            currentBlank[j] = playGame.key.toLowerCase; 
-        }
-        else {
-            lettersGuessed.push();
-            document.getElementById("letters-guessed").innerHTML = playGame.key.toLowerCase();
-            console.log(lettersGuessed);
+            currentBlank[j] = wordToArray[j];
+            document.getElementById("current-blank").innerHTML = currentBlank.join(" ");
+            console.log(currentBlank.join(" "));
+            letter++; 
+            console.log(letter);
+            gameOver--;
+            console.log(gameOver);
         }
     }
+
+    if (letter===0){
+        lettersGuessed.push(playGame.key);
+        document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(" ");
+        numGuesses--;
+        document.getElementById("num-guesses").innerHTML = numGuesses;
+    }
+
+    //checks to see when there are no more letters to find in the current word to end the game
+    if (gameOver===0){
+        stopGame();
+    }
+
+    //what happens when the game ends
+    function stopGame(){
+        alert("Game Over");
+    }
+
+
 }//end of function startGame
-
-
-//id = winCount
-//var = wins
-
-//if there, insert on _
-//continue comparing to end of word
-//if there, decrease number of guesses by 1
-
-//if letter not there, decrease number of guesses by 1
-//add letter to letters already guessed
 
 //if all letters guess before number of guesses is 0
 //increase wins by 1
